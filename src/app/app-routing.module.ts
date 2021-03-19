af99f3f823c984e69ba250b6b9ca1dc5974457b1
verena <9377970+vpchung@users.noreply.github.com>
2021-03-19 02:18:08 -0700

    add initial components and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { ExploreComponent } from './explore/explore.component';
+import { ChallengesComponent } from './challenges/challenges.component';
+import { OrganizationsComponent } from './organizations/organizations.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'explore', component: ExploreComponent },
+  { path: 'challenges', component: ChallengesComponent },
+  { path: 'organizations', component: OrganizationsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
