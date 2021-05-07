eae6e2f19f13e9e44b5e8cc49cf56aa135047a54
Rongrong Chai <rchai@sagebase.org>
2021-05-07 09:58:11 -0400

    seed organizations to database

--- src/app/components/database-seed/database-seed.component.ts
+++ src/app/components/database-seed/database-seed.component.ts
@@ -9,7 +9,7 @@ import {
 } from '@sage-bionetworks/rocc-client-angular';
 import { Tag } from '@sage-bionetworks/rocc-client-angular';
 import tagList from '../../seeds/dream/tags.json';
-
+import orgList from '../../seeds/dream/organizations.json';
 @Component({
   selector: 'rocc-database-seed',
   templateUrl: './database-seed.component.html',
@@ -34,19 +34,34 @@ export class DatabaseSeedComponent implements OnInit {
 
     // const tags: Tag[] = tagList.tags;
     const tags = tagList.tags; // TODO: replace by above line when Tag.tagId is no longer optional
+    const Organizations = orgList.organizations;
 
     const addTags$ = forkJoin(
       tags.map(tag => this.tagService.createTag(tag.tagId, {}))
     );
-
+    
+    const addOrganizations$ = forkJoin(
+      Organizations.map(org => this.organizationService.createOrganization(
+        org.organizationId, 
+        {
+        "name": org.name,
+        "url": org.url,
+        "shortName": org.shortName
+        }))
+    );
+    
     removeDocuments$
       .pipe(
         mergeMap(() => addTags$),
         tap(console.log),
         // mergeMap(() => addPersons$)
       )
+      .pipe(
+        mergeMap(() => addOrganizations$),
+        tap(console.log),
+      )
       .subscribe(res => {
-        console.log('done', res);
+        console.log('done');
       }, err => console.log);
   }
 }
